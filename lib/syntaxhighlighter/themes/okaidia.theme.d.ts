07af49805f55f500830082040e83ba55038d3174
Stefan Bauer <stefan.bauer@n8d.at>
2020-11-30 00:36:45 +0100

    Update okaidia thems

---
+++ lib/syntaxhighlighter/themes/okaidia.theme.d.ts
@@ -0,0 +1,6 @@
+import chalk = require("chalk");
+export const token: typeof token;
+export const background: chalk.Chalk;
+export const backgroundAlt: chalk.Chalk;
+export const toEOL: chalk.Chalk;
+export const inlineSpace: chalk.Chalk;
